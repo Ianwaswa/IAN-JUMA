6adcab3bc02103e3a451ae12ffc75ed7d3703c4b
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-24 14:03:06 +0300

    update code

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,11 @@
 #Manifesting good times ahead
 
-age = 20
+age = 25
 name = "John"
 my_str = "Kindly be %s years old first" % age
 mysecond_str = "Hello %s, you are %s years old. You can join our Night Club" % (name, age)
-for i in range(1, 20):
-    if i >= age:
+for i in range(20, 25):
+    if i >= 18:
         print(my_str)
     else:
         print(mysecond_str)
