ca9c97369553c14986e95d87d256318fce85218a
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-24 13:32:11 +0300

    return number

--- roman_to_int.py
+++ roman_to_int.py
@@ -10,7 +10,10 @@ class Solution:
             "D": 500,
             "M": 1000
         }
-        result = 0
-        for i in range(len(s)):
-            if i > 0 and roman[s[i]] > roman[s[i - 1]]:
-                result += roman[s[i]] - 2 *
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
