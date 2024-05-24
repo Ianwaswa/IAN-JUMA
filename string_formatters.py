0b081c4284935538cc15b830b56d43f7ac6f7ea0
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-24 12:50:24 +0300

    update code

--- string_formatters.py
+++ string_formatters.py
@@ -1,4 +1,12 @@
 car_name = "Volvo"
+other_car = "Toyota"
 car_mileage = "1000"
 response = f"I have a {car_name} car that has {car_mileage} miles on it."
+for car in [car_name, other_car, car_mileage]:
+    if car == car_name:
+        response = response.replace(car, "Volvo")
+    elif car == other_car:
+        response = response.replace(car, "Toyota")
+    elif car == car_mileage:
+        response = response.replace(car, "1000")
 print(response)
