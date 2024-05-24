f8a98a6db91bdcef7f164d7d39968fc205c9bb05
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-24 13:54:46 +0300

    Refactor hello_world.py for better readability and maintainability

--- hello_world.py
+++ hello_world.py
@@ -5,3 +5,7 @@ name = "John"
 my_str = "Kindly be %s years old first" % age
 mysecond_str = "Hello %s, you are %s years old. You can join our Night Club" % (name, age)
 for i in range(1, 20):
+    if i >= age:
+        print(my_str)
+    else:
+        print(mysecond_str)
