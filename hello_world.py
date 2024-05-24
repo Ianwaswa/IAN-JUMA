e4dd591793a938e6dcdd7bca84cee336e2f36640
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-24 14:07:50 +0300

    update code

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,4 @@
 #Manifesting good times ahead
-
-age = 25
-name = "John"
-my_str = "Kindly be %s years old first" % age
-mysecond_str = "Hello %s, you are %s years old. You can join our Night Club" % (name, age)
-for i in range(20, 25):
-    if i >= 18:
-        print(my_str)
-    else:
-        print(mysecond_str)
+name = input("What is your name? ")
+#prompt the user to enter their name
+print("Hello " + name + ", welcome to the world of Python!")
