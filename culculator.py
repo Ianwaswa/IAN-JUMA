41646e5ddd0780d320327d9e376a08461666bde5
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-25 12:40:23 +0300

    update code

--- culculator.py
+++ culculator.py
@@ -10,13 +10,14 @@ difference = num1 - num2
 product = num1 * num2
 quotient = num1 / num2
 remainder = num1 % num2
+exponant = num1 ** num2
 
 print("{} + {} = {}".format(num1, num2, sum))
 print("{} - {} = {}".format(num1, num2, difference))
 print("{} * {} = {}".format(num1, num2, product))
 print("{} / {} = {}".format(num1, num2, quotient))
 print("{} % {} = {}".format(num1, num2, remainder))
-print("{} ** {} = {}".format(num1, num2, num1 ** num2))
+print("{} ** {} = {}".format(num1, num2, exponant))
 
 #If operator is -, subtract the two numbers and print the result based on difference
 #Print the result of the operation
