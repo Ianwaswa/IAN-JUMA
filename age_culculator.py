4f033a53547b62dfb315e4c7b12f373400b7053c
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-25 12:19:37 +0300

    Refactor age_culculator.py for better readability

---
+++ age_culculator.py
@@ -0,0 +1,18 @@
+#Store user's input of two numbers and an operator
+num1, operator, num2 = input("Enter Two Numbers and an Operator: ").split()
+num1, num2 = int(num1), int(num2)
+
+if operator == "+":
+    print("{} + {} = {}".format(num1, num2, num1 + num2))
+
+elif operator == "-":
+    print("{} - {} = {}".format(num1, num2, num1 - num2))
+
+elif operator == "*":
+    print("{} * {} = {}".format(num1, num2, num1 * num2))
+
+elif operator == "/":
+    print("{} / {} = {}".format(num1, num2, num1 / num2))
+
+elif operator == "%":
+    print("{} % {} = {}".format(num1, num2, num1 % num2))
