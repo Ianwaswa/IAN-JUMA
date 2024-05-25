1daa02c03e0d372e54e4f9667ad9c3ab1edf3dd8
Ianwaswa <ianwaswa2019@gmail.com>
2024-05-25 12:10:27 +0300

    update code

--- miles_to_kilometers.py
+++ miles_to_kilometers.py
@@ -1,5 +1,5 @@
 #Ask the user to enter miles
-miles = input(eval("Enter the number of miles: "))
+miles = input("Enter the number of miles: ")
 #Convert the miles to kilometers by multiplying the miles by 1.60934
 kilometers = miles * 1.60934
 print("{} miles is equal to {} kilometers".format(miles, kilometers))
