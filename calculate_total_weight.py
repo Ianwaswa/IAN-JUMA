51d962faf31973bdc21a0212b4e7f2b65d45c6da
Ianwaswa <ianwaswa2019@gmail.com>
2024-06-26 13:29:42 +0300

    update code

---
+++ calculate_total_weight.py
@@ -0,0 +1,8 @@
+# Calculate the total weight of rugby players
+weights = [122, 122,  145, 208, 202, 156,  177, 234, 145, 168, 197, 234, 142,  211, 201, 211, 144, 189, 188, 210, 203, 231, 311, 178, 210, 190, 210, 231, 180, 195, 195, 177, 234, 201, 210, 201, 234, 234, 145, 178, 122, 195, 210, 122, 195, 178, 210, 145, 122]
+total_weight = 0
+for weight in weights:
+    total_weight += weight
+    average_weight = total_weight / len(weights)
+print(f'The total weight of your rugby players is {total_weight}')
+print(average_weight)
