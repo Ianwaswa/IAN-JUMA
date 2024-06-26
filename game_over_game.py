3819291a1710a04a3d7ff0326464f90c8ffaba49
Ianwaswa <ianwaswa2019@gmail.com>
2024-06-26 18:17:20 +0300

    Refactor game_over_game.py to add scoring logic and end the game when score exceeds 100

--- game_over_game.py
+++ game_over_game.py
@@ -4,3 +4,9 @@ game_over = False
 score = 0
 
 while not game_over:
+    score += random.randint(1, 10)
+    print(score)
+        
+    if score > 100:
+        game_over = True
+print("Game Over")
