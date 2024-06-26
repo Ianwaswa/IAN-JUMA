65c17e7f4cdda22983a51f319f6202339e0478ce
Ianwaswa <ianwaswa2019@gmail.com>
2024-06-26 18:46:36 +0300

    Refactor string_templates.py to add a function for repeating a string multiple times

--- string_templates.py
+++ string_templates.py
@@ -1,7 +1,5 @@
-from string import Template
-name = "Harry"
-second_name = "Potter"
-Template = Template("$name and $second_name wish you a happy birthday!")
-birthday_wish = Template.substitute(name=name, second_name=second_name)
-
-print(birthday_wish) # Harry wishes you a happy birthday!
+def string_times(str, n):
+    finalstring = ""
+    for i in range(n):
+        finalstring += str
+    return finalstring
